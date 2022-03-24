7ee41a1e4b608fcd675e818b04d27e714ed5e9bd
hongjoo <dlghdwn10@naver.com>
2022-03-24 19:44:01 +0900

    0324_BJ_S4_Q

---
+++ Baekjoon/스택_큐/BJ_11866_요세푸스문제.java
@@ -0,0 +1,14 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class BJ_11866_요세푸스문제 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine()," ");
+		
+	}
+
+}

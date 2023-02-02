9ecf944d6bdbbe71f4e75f3f024bd0b6cf2c7008
Lee Hongjoo <65024497+Hongjoo22@users.noreply.github.com>
2023-02-02 19:32:51 +0900

    230202_PG_LV3_Date

---
+++ Programmers/SQL/조건별로 분류하여 주문상태 출력하기.sql
@@ -0,0 +1,10 @@
+-- when out_date <= "2022-05-01" then "출고완료"
+-- when out_date > "2022-05-01" then "출고대기"
+SELECT order_id, product_id, date_format(out_date,"%Y-%m-%d"), 
+case
+when date_format(out_date,"%m-%d") <= "05-01" then "출고완료"
+when date_format(out_date,"%m-%d") > "05-01" then "출고대기"
+else "출고미정"
+end
+"출고여부" from food_order
+order by 1

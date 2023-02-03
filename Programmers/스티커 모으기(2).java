ef48ae7856f16c6206daa1c2b7bfc4d8d96e295a
Lee Hongjoo <65024497+Hongjoo22@users.noreply.github.com>
2023-02-03 19:29:00 +0900

    230203_PG_LV3_DP

--- Programmers/스티커 모으기(2).java
+++ Programmers/스티커 모으기(2).java
@@ -1,18 +1,21 @@
 class Solution {
     public int solution(int sticker[]) {
         int N = sticker.length;
+        // 길이가 1인 경우 예외처리
         int answer = sticker[0];
-        int[][] dp = new int[2][N+2];
-        dp[0][2] = sticker[0];
+        int[][] dp = new int[2][N+1];
+        // 첫번째 스티커를 뗀경우
+        dp[0][1] = sticker[0];
         
-        for(int i = 3; i<= N+1;i++){
-            if(i==N+1){
-                dp[0][i] = Math.max(dp[0][i-2],dp[0][i-3]);
-                dp[1][i] = Math.max(dp[1][i-2],dp[1][i-3])+ sticker[i-2];     
+        for(int i = 2; i<= N;i++){
+            // 첫번째 스티커를 뗀경우
+            if(i==N){
+                dp[0][i] = Math.max(dp[0][i-1],dp[0][i-2]);    
             }else{
-                dp[0][i] = Math.max(dp[0][i-2],dp[0][i-3]) + sticker[i-2];
-                dp[1][i] = Math.max(dp[1][i-2],dp[1][i-3]) + sticker[i-2];      
+                dp[0][i] = Math.max(dp[0][i-1],dp[0][i-2] + sticker[i-1]);   
             }
+            // 2번째부터 뗀경우
+            dp[1][i] = Math.max(dp[1][i-1],dp[1][i-2]+ sticker[i-1]); 
            answer = Math.max(answer,Math.max(dp[0][i],dp[1][i]));
         }
         return answer;

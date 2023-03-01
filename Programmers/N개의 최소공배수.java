e62e43754a21e437ae6a461404bff743a57f980b
Lee Hongjoo <65024497+Hongjoo22@users.noreply.github.com>
2023-03-01 13:40:38 +0900

    230301_PG_LV2_유클리드 호제법_GCD_LCM
    공식 잘 알아두자.

--- Programmers/N개의 최소공배수.java
+++ Programmers/N개의 최소공배수.java
@@ -37,3 +37,27 @@ class Solution {
         return answer;
     }
 }
+/**
+
+import java.util.*;
+class Solution {
+    public int solution(int[] arr) {
+        int answer = arr[0];
+        for(int i = 1; i<arr.length; i++){
+            answer = lcm(answer, arr[i]);
+        }
+        return answer;
+    }
+    // 최소 공배수
+    public int lcm(int a, int b){
+        return a * b / gcd(a,b);
+    }
+    // 최대 공약수
+    public int gcd(int a, int b){
+        if(b==0) return a;
+        else return gcd(b, a%b);
+    }
+}
+
+
+*/

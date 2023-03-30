ee62bbe6fa4ebd4f1407f9ef2c4d13991f0e008d
Lee Hongjoo <65024497+Hongjoo22@users.noreply.github.com>
2023-03-30 11:13:24 +0900

    230330_BJ_B5_출력

---
+++ Baekjoon/기타/BJ_11022_A+B-8.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		int N  = Integer.parseInt(br.readLine());
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			int A = Integer.parseInt(st.nextToken());
+			int B = Integer.parseInt(st.nextToken());
+			
+			System.out.println("Case #"+(i+1)+": "+A+" + "+B+" = "+(A+B));
+		}
+	}
+
+}
